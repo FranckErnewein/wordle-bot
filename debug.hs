44c525bf556862a7647409ff078fb82e306cd036
FranckErnewein <franck.ernewein@gmail.com>
2023-05-18 16:16:49 +0200

    save biggest fail to study them

--- debug.hs
+++ debug.hs
@@ -7,24 +7,28 @@ import Control.Monad
 main = do
   fileContent <- readWordsFile
   let allWords = fiveLettersWords fileContent
-  putStrLn $ displayTries $ autoplay allWords (allWords !! 0) []
-  -- putStrLn $ displayTries $ autoplay allWords (allWords !! 1) []
+  let bigFail = ["RARES","SALES","HACHE","SEXES","CASES","SITES","SURES","NAINS","RASES","SAINS","NAINE","MAMES","SOUES","HALES","BITES","DARDE","ZELES","RAPES","TELES","CILLE","DINES","GILLE","TALES","BELER","RACER","CAKES","OILLE","TOUES","HELEE","HELES","CORES","SORES","TATES","RABES","SASSE","MATTE"]
+
+  let alltries = map (\w -> autoplay allWords w []) bigFail
+  putStrLn $ displayTries (alltries !! 1)
   -- print $ sortBy (flip compare `on` fst) $ zip (map (scoreWord allWords) allWords) allWords
   
-  let wordToFind = "CETTE"
-  print $ idealWord allWords []
-  let t0 = [guess "RAIES" wordToFind]
-  putStrLn $ displayTries t0
-  print $ idealWord allWords t0 
+  -- let wordToFind = "COMME"
+  -- print $ idealWord allWords []
+  -- let t0 = [guess "RAIES" wordToFind]
+  -- putStrLn $ displayTries t0
+  -- print $ filterWords allWords t0
+  -- print $ idealWord allWords t0
 
-  let t1 = guess "COULE" wordToFind:t0
-  putStrLn $ displayTries t1
-  print $ idealWord allWords t1 
+  -- let t1 = guess "LEONE" wordToFind:t0
+  -- putStrLn $ displayTries t1
+  -- print $ filterWords allWords t1
+  -- print $ idealWord allWords t1
 
-  let t2 = guess "CHECK" wordToFind:t1
-  putStrLn $ displayTries t2
-  print $ filterWords allWords t2 
-  print $ idealWord allWords t2 
+  -- let t2 = guess "CHECK" wordToFind:t1
+  -- putStrLn $ displayTries t2
+  -- print $ filterWords allWords t2
+  -- print $ idealWord allWords t2
 
   -- let t3 = guess "COMBE" wordToFind:t2
   -- putStrLn $ displayTries t3
@@ -32,4 +36,4 @@ main = do
 
   -- let t4 = guess "COMME" wordToFind:t3
   -- putStrLn $ displayTries t4
-  -- print $ idealWord allWords t4 
+  -- print $ idealWord allWords t4
