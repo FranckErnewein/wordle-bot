0e9b4e8a77390faedd7cae4bcd119961b8c88748
FranckErnewein <franck.ernewein@gmail.com>
2023-05-18 16:14:31 +0200

    retablish play today

--- playToday.hs
+++ playToday.hs
@@ -6,7 +6,6 @@ main = do
   putStrLn "What is the word today ?"
   wordToFind <- getLine
   putStrLn "ok, let's play:"
-  print $ autoplay (fiveLettersWords fileContent) (map toUpper wordToFind) []
-  -- putStrLn $ unlines $ map (map (\(LetterGuess c s) -> c )) (reverse tries)
-  -- putStrLn $ "found in " ++ (show . length) tries ++ " tries"
-  -- putStrLn $ displayTries tries
+  let tries = autoplay (fiveLettersWords fileContent) (map toUpper wordToFind) []
+  putStrLn $ "found in " ++ (show . length) tries ++ " tries"
+  putStrLn $ displayTries tries
